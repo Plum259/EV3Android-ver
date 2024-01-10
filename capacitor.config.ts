7c087214d07452b6605de7df50efab31f1f96f9d
Plum259 <plumcoyote@gmail.com>
2024-01-10 15:57:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'evaluacion3.intento2',
+  appName: 'evaluacion3-intento2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
